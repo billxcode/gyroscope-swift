9f45c41b0cb574b154506c748e50a44fd9ab366f
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2019-07-13 18:49:23 +0700

    Initial Commit

---
+++ GyroScopeSensor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GyroScopeSensor
+//
+//  Created by Bill Tanthowi Jauhari on 13/07/19.
+//  Copyright © 2019 Batavia Hack Town. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
